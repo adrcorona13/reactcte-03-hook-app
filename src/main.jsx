7ab4bbda93a4aed112284b1b20af57dcc2199372
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-31 13:10:52 -0600

    useEffectMain

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 // import { CounterApp } from './assets/01-useState/CounterApp'
 import { CounterWithCustomHook } from './assets/01-useState/CounterWithCustomHook'
+import { SimpleForm } from './assets/02-useEffect/SimpleForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CounterWithCustomHook />
-    <CounterWithCustomHook />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <SimpleForm/>
+  // </React.StrictMode>,
 )
