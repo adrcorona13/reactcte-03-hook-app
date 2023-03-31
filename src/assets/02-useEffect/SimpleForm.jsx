f8e40643cd9b5c6463d0f2d7df26b3d8225640e7
Adrian Pineda <adrian.pc13@hotmail.com>
2023-03-31 13:10:34 -0600

    useEffect y dependencias

---
+++ src/assets/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'usuario',
+        email: 'usuario1@email.com'
+    });
+
+    const {username, email} = formState;
+
+    const onInputChange = ({target}) =>{
+        const {value, name} = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+    
+    useEffect(() => {
+        console.log('useEffect called!')
+    }, [])
+    
+    useEffect(() => {
+        console.log('formState change!')
+    }, [formState])
+    
+    useEffect(() => {
+        console.log('email change!')
+    }, [email])
+
+    
+
+    return (
+        <>
+            <h1>Formulario Simple</h1>
+            <hr />
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="usuario@correo.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
