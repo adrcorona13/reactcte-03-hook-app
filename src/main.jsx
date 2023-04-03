9d8a983e6976e36f9d0fb6372fd7eec091fbfb8c
Adrian Pineda <adrian.pc13@hotmail.com>
2023-04-03 15:54:36 -0600

    crear form con hook

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,10 @@ import './index.css'
 // import { CounterApp } from './assets/01-useState/CounterApp'
 import { CounterWithCustomHook } from './assets/01-useState/CounterWithCustomHook'
 import { SimpleForm } from './assets/02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './assets/02-useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <SimpleForm/>
+    <FormWithCustomHook/>
   // </React.StrictMode>,
 )
