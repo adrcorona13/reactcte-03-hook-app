bb222cd53b129aeee42e28772a13a4ad845b954a
Adrian Pineda <adrian.pc13@hotmail.com>
2023-04-04 00:07:03 -0600

    memo

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
+import { Memorize } from './assets/06-memos/Memorize'
 // import { CounterApp } from './assets/01-useState/CounterApp'
 // import { CounterWithCustomHook } from './assets/01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './assets/02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './assets/02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './assets/03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './assets/04-useRef/FocusScreen'
-import { Layout } from './assets/05-useLayoutEffect/Layout'
+// import { Layout } from './assets/05-useLayoutEffect/Layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout/>
+    <Memorize/>
   // </React.StrictMode>,
 )
