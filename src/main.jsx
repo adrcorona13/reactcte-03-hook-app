6238e413c7f8e7ef56ebf911b0ca81ec1c5eb77a
Adrian Pineda <adrian.pc13@hotmail.com>
2023-04-03 23:12:38 -0600

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 // import { CounterApp } from './assets/01-useState/CounterApp'
-import { CounterWithCustomHook } from './assets/01-useState/CounterWithCustomHook'
-import { SimpleForm } from './assets/02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './assets/02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './assets/03-examples/MultipleCustomHooks'
+// import { CounterWithCustomHook } from './assets/01-useState/CounterWithCustomHook'
+// import { SimpleForm } from './assets/02-useEffect/SimpleForm'
+// import { FormWithCustomHook } from './assets/02-useEffect/FormWithCustomHook'
+// import { MultipleCustomHooks } from './assets/03-examples/MultipleCustomHooks'
+import { FocusScreen } from './assets/04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks/>
+    <FocusScreen/>
   // </React.StrictMode>,
 )
