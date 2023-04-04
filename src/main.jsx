481a27ce996c4e09406a160319d2824cd56e70db
Adrian Pineda <adrian.pc13@hotmail.com>
2023-04-03 23:56:28 -0600

    useLayoutEffect

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,11 @@ import './index.css'
 // import { SimpleForm } from './assets/02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './assets/02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './assets/03-examples/MultipleCustomHooks'
-import { FocusScreen } from './assets/04-useRef/FocusScreen'
+// import { FocusScreen } from './assets/04-useRef/FocusScreen'
+import { Layout } from './assets/05-useLayoutEffect/Layout'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <FocusScreen/>
+    <Layout/>
   // </React.StrictMode>,
 )
