6703a7f6781914c1c73815d673f8f08d8d30a563
Adrian Pineda <adrian.pc13@hotmail.com>
2023-04-05 00:39:15 -0600

    iniciar con el TodoList

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -14,11 +14,12 @@ import { Padre } from './07-tarea-memo/Padre'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
+// import './08-useReducer/intro-reducer';
 
-import './08-useReducer/intro-reducer';
+import { TodoApp } from './08-useReducer/TodoApp'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <Padre/>
-//   // </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoApp/>
+  // </React.StrictMode>,
+)
