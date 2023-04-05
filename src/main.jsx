5703201220f8f20489fa7d9da066746f15c221f0
Adrian Pineda <adrian.pc13@hotmail.com>
2023-04-04 19:50:32 -0600

    preparacion para reducer

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 
 import './index.css'
-import { Memorize } from './assets/06-memos/Memorize'
-import { MemoHook } from './assets/06-memos/MemoHook'
-import { CallbackHook } from './assets/06-memos/CallbackHook'
-import { Padre } from './assets/07-tarea-memo/Padre'
-// import { CounterApp } from './assets/01-useState/CounterApp'
-// import { CounterWithCustomHook } from './assets/01-useState/CounterWithCustomHook'
-// import { SimpleForm } from './assets/02-useEffect/SimpleForm'
-// import { FormWithCustomHook } from './assets/02-useEffect/FormWithCustomHook'
-// import { MultipleCustomHooks } from './assets/03-examples/MultipleCustomHooks'
-// import { FocusScreen } from './assets/04-useRef/FocusScreen'
-// import { Layout } from './assets/05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLayoutEffect/Layout'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre/>
-  // </React.StrictMode>,
-)
+import './08-useReducer/intro-reducer';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre/>
+//   // </React.StrictMode>,
+// )
