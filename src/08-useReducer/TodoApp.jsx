4fbbebd45daa5b175e2780aeb4e23687e71d1535
Adrian Pineda <adrian.pc13@hotmail.com>
2023-05-08 19:09:50 -0600

    borrar todo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -36,13 +36,20 @@ export const TodoApp = () => {
     dispatchTodo(action);
   }
 
+  const handleDeleteTodo = (id) => {
+    dispatchTodo({
+      type: '[TODO] Remove',
+      payload: id
+    })
+  }
+
   return (
     <>
       <h1>TodoApp 10 <small>pendientes: 2</small></h1>
       <hr />
       <div className="row">
         <div className="col-7">
-            <TodoList todos={todos}/>
+            <TodoList todos={todos} onDeleteTodo={handleDeleteTodo}/>
         </div>
         <div className="col-5">
           <h4>Agregar tarea</h4>
--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -3,7 +3,8 @@ export const todoReducer = (initialState = [], action = {}) => {
     switch (action.type) {
         case '[TODO] Add':
             return [...initialState, action.payload]
-
+        case '[TODO] Remove':
+            return initialState.filter(todo => todo.id !== action.payload);
         default:
             return initialState;
     }
