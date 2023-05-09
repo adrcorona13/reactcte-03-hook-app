bba4fad1b8d37df85c92451c613f10b4a4e9d82a
Adrian Pineda <adrian.pc13@hotmail.com>
2023-05-08 19:25:55 -0600

    completar todo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -43,13 +43,23 @@ export const TodoApp = () => {
     })
   }
 
+  const handleToggleTodo = (id) => {
+    dispatchTodo({
+      type: '[TODO] Toggle',
+      payload: id
+    })
+  }
+
   return (
     <>
       <h1>TodoApp 10 <small>pendientes: 2</small></h1>
       <hr />
       <div className="row">
         <div className="col-7">
-            <TodoList todos={todos} onDeleteTodo={handleDeleteTodo}/>
+            <TodoList 
+              todos={todos}  
+              onDeleteTodo={handleDeleteTodo}
+              onToggleTodo={handleToggleTodo}/>
         </div>
         <div className="col-5">
           <h4>Agregar tarea</h4>
